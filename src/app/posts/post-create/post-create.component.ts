97a5871504fc329be03feeeb0539a25ed964d4c3
satish <sj8390613529@gmail.com>
2023-04-21 21:45:08 +0530

    work on edit post function

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,18 +1,43 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 import { PostsService } from "../posts.service";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Post } from "../post.model";
 
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
   styleUrls: ["./post-create.component.css"]
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   enteredTitle = "";
   enteredContent = "";
+  private postId:string;
+  private mode = "create";
+  private post:Post;
 
-  constructor(public postsService: PostsService) {}
+
+  constructor(public postsService: PostsService, public route:ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((param:ParamMap)=>
+    {
+      if (param.has('postId')) {
+        this.postId = param.get('postId');
+        this.mode = 'edit';
+       this.post =  this.postsService.getPost(this.postId);
+      }
+      else
+      {
+        this.mode='create';
+        this.postId = null;
+      }
+    })
+  }
+
+
+
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
